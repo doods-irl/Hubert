805b5164107a796d208079bf96e1902b0d7b3830
doods-irl <max.vinten@gmail.com>
2023-12-04 22:15:35 +1300

    new stuff

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const moment = require('moment');
 const express = require('express');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
-const fs = require('fs');
+const fs = require('fs').promises;
 const { constants } = require('fs');
 const { Mutex } = require('async-mutex');
 const mutex = new Mutex();
@@ -34,29 +34,34 @@ server.use(express.static('mobile'));
 server.use(express.static('assets'));
 server.use(express.json());
 
-// Serve browser.html for browser clients
+// Serve home.html for mobile clients
 server.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'mobile', 'home.html'));
 });
 
+// Activate server for mobile clients
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
+// Create the electron Window
 electronApp.whenReady().then(createWindow);
 
+// Electron catch-all for MacOS
 electronApp.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     electronApp.quit();
   }
 });
 
+// Electron error handling
 electronApp.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
 
+// Write people back to the json
 server.post('/save-people', async (req, res) => {
   const newNames = req.body; // Array of new names
 
@@ -88,6 +93,7 @@ server.post('/save-people', async (req, res) => {
   }
 });
 
+// Get people from the JSON
 server.get('/get-people', async (req, res) => {
     const release = await mutex.acquire();
     const filePath = './store/people.json';
@@ -116,7 +122,7 @@ server.get('/get-people', async (req, res) => {
     }
 });
 
-
+//Delete people from the JSON
 server.delete('/delete-people/:id', async (req, res) => {
   const personId = req.params.id;
   console.log(`Deleting person with ID: ${personId}`);
@@ -145,3 +151,202 @@ server.delete('/delete-people/:id', async (req, res) => {
   }
 });
 
+// TASK ACTIONS
+server.post('/save-task', async (req, res) => {
+  const newTask = req.body;
+  const filePath = './store/tasks.json';
+
+  const release = await mutex.acquire();
+
+  try {
+      let tasks = await checkFile(filePath);
+
+      // Assign a unique taskId
+      newTask.taskId = uuidv4();
+
+      // Add the new task to the appropriate category
+      if (!tasks[newTask.taskCategory]) {
+          tasks[newTask.taskCategory] = [];
+      }
+      tasks[newTask.taskCategory].push(newTask);
+
+      console.log(newTask);
+      console.log(tasks);
+
+      // Write the updated tasks to the file
+      await fs.promises.writeFile(filePath, JSON.stringify(tasks), 'utf8');
+      console.log("JSON file with tasks has been updated.");
+      res.json({ message: 'Task Saved' });
+  } catch (error) {
+      console.error("An error occurred.", error);
+      res.status(500).json({ error: "Internal server error" });
+  } finally {
+      release();
+  }
+});
+
+// Get people from the JSON
+server.get('/get-tasks', async (req, res) => {
+  const release = await mutex.acquire();
+  const filePath = './store/tasks.json';
+
+  try {
+      const tasks = await checkFile(filePath, []);
+      console.log(tasks);
+      res.json(tasks);
+  } catch (err) {
+      console.error("Error accessing/reading file", err);
+      res.status(500).json({ error: "Internal server error" });
+  } finally {
+      release();
+  }
+});
+
+
+server.delete('/delete-task/:id', async (req, res) => {
+  const taskId = req.params.id;
+  console.log(`Deleting task with ID: ${taskId}`);
+
+  // Acquire a lock before reading and writing to the file
+  const release = await mutex.acquire();
+
+  try {
+      // Read the current contents of the file
+      let data = await fs.promises.readFile('./store/tasks.json', 'utf8');
+      let tasks = JSON.parse(data);
+
+      // Remove the task with the given ID from each category
+      Object.keys(tasks).forEach(category => {
+          tasks[category] = tasks[category].filter(task => task.taskId !== taskId);
+      });
+
+      // Write the updated tasks back to the file
+      await fs.promises.writeFile('./store/tasks.json', JSON.stringify(tasks), 'utf8');
+      console.log("JSON file with tasks has been updated.");
+      res.json({ message: 'Task deleted successfully' });
+  } catch (err) {
+      console.error("Error processing file", err);
+      res.status(500).json({ error: "Internal server error" });
+  } finally {
+      // Release the lock
+      release();
+  }
+});
+
+
+async function checkFile(filePath, initialContent = {}) {
+  try {
+      await fs.promises.access(filePath, constants.F_OK);
+      let data = await fs.promises.readFile(filePath, 'utf8');
+      return JSON.parse(data);
+  } catch (accessErr) {
+      if (accessErr.code === 'ENOENT') {
+          console.log("File not found. Creating a new file.");
+          await fs.promises.writeFile(filePath, JSON.stringify(initialContent), 'utf8');
+          return initialContent;
+      } else {
+          throw accessErr;
+      }
+  }
+}
+
+async function processTasks() {
+  const sourceFile = path.join(__dirname, "./store/tasks.json");
+  const destFile = path.join(__dirname, "./store/tasks-processed.json");
+
+  try {
+    // Read source file
+    const rawData = await fs.readFile(sourceFile, 'utf8');
+    const tasks = JSON.parse(rawData);
+
+    // Process tasks
+    const processedTasks = processAllTasks(tasks);
+
+    // Write to destination file
+    await fs.writeFile(destFile, JSON.stringify(processedTasks, null, 2), 'utf8');
+
+  } catch (err) {
+    console.error("Error accessing/reading file:", err);
+    throw err; // or handle it as needed
+  }
+}
+
+function processAllTasks(tasks) {
+  const processedTasks = [];
+
+  for (const category in tasks) {
+    tasks[category].forEach(task => {
+      const taskRepetitions = processTask(task);
+      processedTasks.push(...taskRepetitions);
+    });
+  }
+
+  return processedTasks;
+}
+
+function processTask(task) {
+  const repetitions = [];
+  let currentDate = moment.utc(task.date);
+  let count = 0;
+
+  while (shouldContinueRepeating(task, currentDate, count)) {
+      const standardizedTask = {
+        ...task, // Spread syntax to copy all properties from the original task
+        date: currentDate.format('YYYY-MM-DD'), // Overwrite date for each repetition
+      };
+      repetitions.push(standardizedTask);
+
+      let newDate = getNextOccurrenceDate(currentDate, task.repeatType);
+      if (!newDate.isValid()) {
+          console.error("Invalid new date encountered, stopping repetition:", newDate);
+          break;
+      }
+      currentDate = newDate;
+      count++;
+  }
+
+  return repetitions;
+}
+
+
+function shouldContinueRepeating(task, currentDate, count) {
+  console.log("Current date in shouldContinueRepeating:", currentDate);
+
+  if (!currentDate.isValid()) {
+      console.error("Invalid date encountered in shouldContinueRepeating:", currentDate);
+      return false;
+  }
+
+  const maxDate = moment().add(90, 'days');
+
+  if (task.repeatTimes === 'forever') {
+      return currentDate.isSameOrBefore(maxDate);
+  } else if (task.repeatTimes === 'limited') {
+      return (count < task.repeatTimesNumber) && currentDate.isSameOrBefore(maxDate);
+  }
+
+  // If repeatTimes is neither 'forever' nor 'limited', or if some other case needs handling.
+  return false;
+}
+
+function getNextOccurrenceDate(currentDate, repeatType) {
+  let nextDate = currentDate.clone();
+
+  switch (repeatType) {
+      case 'daily':
+          nextDate.add(1, 'days');
+          break;
+      case 'weekly':
+          nextDate.add(7, 'days');
+          break;
+      case 'month':
+          nextDate.add(1, 'month');
+          break;
+      // ... other cases ...
+  }
+
+  console.log("Next occurrence date:", nextDate.format('YYYY-MM-DD'));
+  return nextDate;
+}
+
+processTasks();
